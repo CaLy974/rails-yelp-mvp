45a11efaf4ea28a838c2d92129f8f67a0f2d6bcb
caroline bousquet gh api caroline.bbr@outlook.fr <caroline.bbr@outlook.fr>
2022-11-16 15:47:08 +0100

    done

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,20 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def edit
+  end
+
+  # def destroy
+    # @restaurant.destroy redirect_to restaurants_url
+  # end
+end
